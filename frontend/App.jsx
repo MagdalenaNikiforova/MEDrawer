50aa21a6c6dc33acf5da5e93248cbe6c2b825bed
StefyVP <80509357+StefyVP@users.noreply.github.com>
2025-03-21 21:12:04 +0200

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import Home from "./Home";
 import AddMedicine from "./AddMedicine";
+import AddCalendar from './AddCalendar';
 import Daily from "./Daily";
 import Weekly from "./Weekly";
 
@@ -10,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/add-medicine" element={<AddMedicine />}/>
+        <Route path="/add-calendar" element={<AddCalendar />}/>
         <Route path="/daily" element={<Daily />}/>
         <Route path="/weekly" element={<Weekly />}/>
       </Routes>

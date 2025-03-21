3272909366eeee951e95e2110b4cacd4a27d1048
StefyVP <80509357+StefyVP@users.noreply.github.com>
2025-03-21 21:28:23 +0200

    Add files via upload

---
+++ frontend/Weekly.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Weekly() {
+  
+  return (
+    <div className="container">
+        <div className='row'>
+            <div className='col mt-5'>
+            
+            <h1>Weekly title</h1>
+
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default Weekly;
